21604a14af77c5cd9ac75f9c4421e7786e76010b
Muharrem ERİN <erin.muharrem@gmail.com>
2025-05-08 03:33:26 +0300

    #initial-commit

--- src/src/main/java/com/mewebstudio/translatable/dto/response/AbstractBaseResponse.java
+++
@@ -1,9 +0,0 @@
-package com.mewebstudio.translatable.dto.response;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonIgnoreProperties(ignoreUnknown = true)
-public abstract class AbstractBaseResponse {
-}
